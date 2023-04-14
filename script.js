07de8eec6379d2b4a99c3282733f333f56e76fa6
KONDILAKSHMI <130298339+KONDILAKSHMI@users.noreply.github.com>
2023-04-14 12:26:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const calculateTemp = () => {
+    const inputTemp = document.getElementById('temperature').value;
+
+    const tempSelected = document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
+
+    const celToFah = (cel) =>{
+        let fahrenheit = ((cel * 9 / 5) + 32).toFixed(1);
+        return fahrenheit;
+    }
+
+    const fahToCel = (fah) =>{
+        let celsius = ((fah - 32) * 5 / 9).toFixed(1);
+        return celsius;
+
+    }
+
+    if (valueTemp == 'celcius') {
+        document.getElementById("answer").innerHTML = celToFah(inputTemp) + "&#176; Fahrenheit";
+
+    }
+
+    else {
+        document.getElementById("answer").innerHTML = fahToCel(inputTemp) + "&#176; Celsius";
+    }
+}
